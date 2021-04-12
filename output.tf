4da25aeec0ae742ffacae6302dc479866dd2a8fa
Bruno Portilho <brunovrportilho@gmail.com>
2021-04-11 22:58:26 -0300

    Output update

--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
-output "Instance_IP_Adress" {
-  value = "${oci_core_instance.webserver1.public_ip}"
+output "webserver1_IP_Adress" {
+  value = "oci_core_instance.webserver1.public_ip"
+}
+output "webserver2_IP_Adress" {
+  value = "oci_core_instance.webserver1.public_ip"
 }
